37ce21931546bb8df88fa00cf0f759196079cd81
Stephen Fay <dcxstephen@gmail.com>
2023-08-09 20:41:51 -0400

    ledger tested v0 requirements work

--- ledger.py
+++ ledger.py
@@ -1,4 +1,4 @@
-#!/opt/homebrew/Caskroom/miniforge/base/bin/python
+#!/Users/steve/Documents/code/simple_ledger/ledgerenv/bin/python
 """
 This code enables you to add things to a ledger or display account summary.
 There are two modes: `add` and `show`. 
@@ -19,12 +19,12 @@ MODES = ('add','show')
 # Valid display options
 DISP = ('full','head','summary')
 # Path to ledger .csv database file
-LEDGER_PATH = '/Users/steve'
+LEDGER_PATH = '/Users/steve/.ledger.csv'
 
 
 def append_row(timestamp:str,item:str,val:int,comment:str=''):
     with open(LEDGER_PATH,'a') as f:
-        f.write(f'{timestamp},{item},{val},{comment}')
+        f.write(f'{timestamp},{item},{val},{comment}\n')
     return
 
 def display_full():
@@ -36,52 +36,59 @@ def display_head(n:int=10):
         line=f.readline()
         for i in range(n):
             print(line)
+            line=f.readline()
             if line=='': break
     return
 
 def display_summary():
-    print("Summary not yet implemented, calling display_head!")
-    display_head() # dummy
+    print("Summary not yet implemented, calling balance!")
+    balance() # dummy
     return 
+
+def balance():
+    df=read_csv(LEDGER_PATH,header=None)
+    print(df[2].sum())
 
 if __name__=="__main__":
     import sys
-    from optparse import OptionParser
+    from argparse import ArgumentParser
     from datetime import datetime as dt
+    from pandas import read_csv
 
-    p = OptionParser()
-    p.set_usage('ledger.py <>')
+    p = ArgumentParser(prog='ledger',
+                       description='keep track of spending & earning')
 
-    p.add_option('-i','--item',dest='item',type='str',
+    p.add_argument('mode',type=str,
+                   help=f'Mode must be in {MODES}')
+    p.add_argument('-i','--item',dest='item',type=str,
                  help=f'Item must be in {ITEMS}')
-    p.add_option('-v','--dollar_value',dest='dollar_value',
-                 type='int',default=0,
+    p.add_argument('-v','--dollar_value',dest='dollar_value',
+                 type=int,default=0,
                  help=f'Dollar value earned.')
-    p.add_option('-c','--comment',dest='comment',type='str',default='',
+    p.add_argument('-c','--comment',dest='comment',type=str,default='',
                  help='')
-    p.add_option('-d','--disp',dest='display_mode',type='str',
+    p.add_argument('-d','--disp',dest='display_mode',type=str,
                  default='summary',
-                 help='')
+                 help=f'Disp must be in {DISP}')
 
-    ops, args = p.parse_args(sys.argv[1:])
-    assert args!=[], f'must supply argument `mode` in {MODES}'
-    mode = args[0]
-    assert mode in MODES, f'could not find {mode} in {MODES}'
+    args = p.parse_args()
+    print(f"args {args}\n")
+    assert args.mode in MODES, f'could not find {args.mode} in {MODES}'
     # conditional flow
-    if mode=="add":
-        assert ops.item in ITEMS, f'could not find {ops.item} in {ITEMS}'
-        assert ops.dollar_value==0, f'dollar value must be set with -v,\
+    if args.mode=="add":
+        assert args.item in ITEMS, f'could not find {args.item} in {ITEMS}'
+        assert args.dollar_value!=0, f'dollar value must be set with -v,\
                 --val to a non-zero value'
         timestamp = dt.now().__str__()
-        append_row(timestamp,ops.item,ops.dollar_value,ops.comment)
-    elif mode=="show":
-        assert ops.display_mode in DISP, f'could not find \
-                {ops.display_mode} in DISP'
-        if ops.display_mode == 'full': 
+        append_row(timestamp,args.item,args.dollar_value,args.comment)
+    elif args.mode=="show":
+        assert args.display_mode in DISP, f'could not find \
+                {args.display_mode} in DISP'
+        if args.display_mode == 'full': 
             display_full()
-        elif ops.display_mode == 'head': 
+        elif args.display_mode == 'head': 
             display_head()
-        elif ops.display_mode == 'summary':
+        elif args.display_mode == 'summary':
             display_summary()
         else:
             raise Exception('This error message should never display.')
